2b2d1e6c648edd8bee154404d0f6d604ae36ecd2
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-08 13:29:36 -0600

    Finished markup for Header.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-{/* Header */}
 <Header/>
+
 {/* Sidebar */}
 
 {/* React-Router -> Chat Screen */}
-
-
    </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import "../Styles/Header.css";
+import {Avatar} from '@material-ui/core';
+import { AccessTime } from "@material-ui/icons";
+import { Search } from "@material-ui/icons";
+import { HelpOutline } from "@material-ui/icons";
 
 function Header() {
-    return (
-        <div>
-            This is the Header
-        </div>
-    )
+  return (
+    <div className="header">
+      <div className="header_left">
+        <Avatar
+        className='header-avatar'
+        alt='Jack Rigan'
+        src=''
+        />
+        <AccessTime/>
+      </div>
+      <div className="header_search">
+        <Search/>
+        <input placeholder='Search Jack Rigan'/>
+      </div>
+      <div className="header_right">
+         <HelpOutline/>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
