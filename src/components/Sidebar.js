a100c850a8105aeb82fe643afa4bb4b7a2a9f79f
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-08 14:42:13 -0600

    formatted code

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,40 +1,40 @@
-import React from 'react'
+import React from "react";
 import "../Styles/Sidebar.css";
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-import CreateIcon from '@material-ui/icons/Create';
-import SidebarOption from './SidebarOption';
-import InsertCommentIcon from '@material-ui/icons/InsertComment';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import AppsIcon from '@material-ui/icons/Apps';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import CreateIcon from "@material-ui/icons/Create";
+import SidebarOption from "./SidebarOption";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 function Sidebar() {
-    return (
-        <div className='sidebar'>
-          <div className='sidebar_header'>
-              <div className='sidebar_info'>
-        <h2>Coders Club</h2>
-              <h3><FiberManualRecordIcon/>
-              Jack Rigan
-              </h3>
-              </div>
-            <CreateIcon/>
-          </div>
-          <SidebarOption Icon={InsertCommentIcon} title='Threads'/>
-          <SidebarOption title='Gaming'/>
-          <SidebarOption Icon={InboxIcon} title='Mentions & Threads'/>
-          <SidebarOption Icon={DraftsIcon} title='Saved Items' />
-          <SidebarOption Icon={BookmarkBorderIcon} title ='Channel browser'/>
-          <SidebarOption Icon={PeopleAltIcon} title='People & user groups' />
-          <SidebarOption Icon={AppsIcon} title='Apps'/>
-          <SidebarOption Icon={FileCopyIcon} title='File Browser'/>
-          <SidebarOption Icon={ExpandLessIcon}/>
-
+  return (
+    <div className="sidebar">
+      <div className="sidebar_header">
+        <div className="sidebar_info">
+          <h2>Coders Club</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            Jack Rigan
+          </h3>
         </div>
-    )
+        <CreateIcon />
+      </div>
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption title="Gaming" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & Threads" />
+      <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+      <SidebarOption Icon={ExpandLessIcon} />
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
