81101fbedfa1171d50c8934a9189df532a9b9343
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-08 14:41:51 -0600

    Added more sidebarOptions

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,8 +4,13 @@ import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
 import SidebarOption from './SidebarOption';
 import InsertCommentIcon from '@material-ui/icons/InsertComment';
-
-
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 function Sidebar() {
     return (
         <div className='sidebar'>
@@ -20,6 +25,14 @@ function Sidebar() {
           </div>
           <SidebarOption Icon={InsertCommentIcon} title='Threads'/>
           <SidebarOption title='Gaming'/>
+          <SidebarOption Icon={InboxIcon} title='Mentions & Threads'/>
+          <SidebarOption Icon={DraftsIcon} title='Saved Items' />
+          <SidebarOption Icon={BookmarkBorderIcon} title ='Channel browser'/>
+          <SidebarOption Icon={PeopleAltIcon} title='People & user groups' />
+          <SidebarOption Icon={AppsIcon} title='Apps'/>
+          <SidebarOption Icon={FileCopyIcon} title='File Browser'/>
+          <SidebarOption Icon={ExpandLessIcon}/>
+
         </div>
     )
 }
