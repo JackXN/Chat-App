64b3a784137396d8a38f313aae8644ab66218990
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-08 15:37:02 -0600

    Initialized firebase db connection

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAWp99ATu7DhCxt5O-2DuqrqOwXnpRx6pE",
     authDomain: "chatapp-d95b6.firebaseapp.com",
@@ -8,3 +11,13 @@ const firebaseConfig = {
     appId: "1:517082277084:web:3b188bf40209d440a48019",
     measurementId: "G-FNNQ5WSENL"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+
+  export {auth, provider};
+
+  export default db;
