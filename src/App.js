da79be5fbd748c880c08c12735db44957b845753
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-08 13:46:23 -0600

    Created sidebar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 
 import './App.css';
 import Header from './components/Header';
-
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
 <Header/>
+<div className='app_body'>
+<Sidebar/>
 
 {/* Sidebar */}
-
+</div>
 {/* React-Router -> Chat Screen */}
    </div>
   );
