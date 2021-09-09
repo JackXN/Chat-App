4f1755259cf69561544243b8e55265e05781adec
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-08 19:47:52 -0600

    Created Login card

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import "../Styles/Login.css";
-
+import { Button } from '@material-ui/core';
 
 function Login() {
     return (
         <div className='login'>
-            <h1>Login page</h1>
+            <div className='login_container'>
+            <img src='https://avatars.githubusercontent.com/u/73765650?v=4' alt=''/>
+        <h1>Sign In To Superizee Chat</h1>
+<p>superizee.chat.com</p>
+<Button>Sign In With Google</Button>
+            </div>
         </div>
     )
 }
