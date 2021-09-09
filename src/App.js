3c4b707896cae75811d8e1fc5faf3b6eee2a4e00
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-08 19:56:54 -0600

    Signin button with google auth

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 
 const [user,setUser] = useState(null);
 
+
   return (
     <div className="App">
       <Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import "../Styles/Login.css";
 import { Button } from '@material-ui/core';
-
+import {auth,provider} from '../firebase';
 function Login() {
+
+
+const signIn = (e) => {
+auth
+.signInWithPopup(provider)
+.then(result => {
+    console.log(result)
+})
+.catch(error => {
+    alert(error.message);
+})
+}
     return (
         <div className='login'>
             <div className='login_container'>
             <img src='https://avatars.githubusercontent.com/u/73765650?v=4' alt=''/>
         <h1>Sign In To Superizee Chat</h1>
 <p>superizee.chat.com</p>
-<Button>Sign In With Google</Button>
+<Button onClick={signIn}>Sign In With Google</Button>
             </div>
         </div>
     )
