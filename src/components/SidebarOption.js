bdff328db8608c04f138b9786438564a15481f44
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-15 13:15:56 -0600

    bump

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,43 +1,45 @@
-import React from 'react'
-import "../Styles/SidebarOption.css"
-import {useHistory} from 'react-router-dom';
-import db from '../firebase';
+import React from "react";
+import "../Styles/SidebarOption.css";
+import { useHistory } from "react-router-dom";
+import db from "../firebase";
 
-function SidebarOption({Icon, title, addChannelOption, id}) {
+function SidebarOption({ Icon, title, addChannelOption, id }) {
+  const history = useHistory();
 
-const history = useHistory();
-
-const selectChannel = () => {
-    if(id) {
-        history.push(`/room/${id}`)
-    }else {
-        history.push(title);
+  const selectChannel = () => {
+    if (id) {
+      history.push(`/room/${id}`);
+    } else {
+      history.push(title);
     }
-}
+  };
 
-const addChannel = () => {
-const channelName = prompt('Please enter the channel name');
-if(channelName) {
-    db.collection('rooms').add({
+  const addChannel = () => {
+    const channelName = prompt("Please enter the channel name");
+    if (channelName) {
+      db.collection("rooms").add({
         name: channelName,
-    })
-}
+      });
+    }
+  };
+
+  return (
+    <div
+      className="sidebarOption"
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
+      {/* /if the icon exists display */}
+      {Icon && <Icon className="sidebarOption_icon" />}
+      {Icon ? (
+        <h3>{title}</h3>/
+      ) : (
+        <h3 className="sidebarOption_channel">
+          <span className="sidebarOption_hash">#</span>
+          {title}
+        </h3>
+      )}
+    </div>
+  );
 }
 
-    return (
-        <div className='sidebarOption' onClick={addChannelOption ? addChannel : selectChannel}>
-            {/* /if the icon exists display */}
-         {Icon && <Icon className='sidebarOption_icon'/>}
-         {Icon ? (
-             <h3>{title}</h3>
-         ): (
-             <h3 className='sidebarOption_channel'>
-                 <span className='sidebarOption_hash'>#</span>
-                 {title}
-             </h3>
-         )}
-        </div>
-    )
-}
-
-export default SidebarOption
+export default SidebarOption;
